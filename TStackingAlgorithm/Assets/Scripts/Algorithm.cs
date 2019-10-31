90cc2508294b25b235a381f08b519d452d030add
Sijin Chen <fvjackchen@gmail.com>
2019-10-30 18:20:50 -0700

    Upload the TStacking Algorithm Demo
    Need Eric to fullfill the algorithm to calculation the position and index of waitting queue.

---
+++ TStackingAlgorithm/Assets/Scripts/Algorithm.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Algorithm : MonoBehaviour {
+
+    //Prefabs of container with anchor
+    public GameObject box;
+
+    private Transform container;
+    private Vector3 containerXYZ;
+    private List<Vector3> waitingQueue = new List<Vector3>();
+
+    public void SetContainer(Vector3 containerXYZ)
+    {
+        this.containerXYZ = containerXYZ;
+        //This component doesn't need for right now, maybe need for future, Dont worry about it
+        this.container = GameObject.FindGameObjectWithTag("Container").GetComponent<Transform>();
+    }
+
+    public void SetBox(Vector3 boxXYZ)
+    {
+        waitingQueue.Add(boxXYZ);
+
+        /*Uncomment if it is need
+        if(waitingQueue.Count > 5)
+            CalculatePosition();
+        */
+        CalculatePosition();
+    }
+
+	public void CalculatePosition()
+    {
+        Vector3 boxPosition = Vector3.zero;
+        int waitingQueueIndex = 0;
+        //This is for the algorithm to calculate the position
+
+
+        GenerateBox(boxPosition, waitingQueueIndex);
+    }
+
+    private void GenerateBox(Vector3 position, int waitingQueueIndex)
+    {
+        GameObject boxCreated = box;
+        boxCreated.transform.position = new Vector3(position.x, position.y, -position.z);
+        boxCreated.transform.localScale = waitingQueue[waitingQueueIndex];
+        GameObject.Instantiate(boxCreated, container, true);
+    }
+}
---
+++ TStackingAlgorithm/Assets/Scripts/InputController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputController : MonoBehaviour
+{
+    //Recieve the data of the container or box X, Y, Z
+    public InputField x;
+    public InputField y;
+    public InputField z;
+
+    //Prefabs of container with anchor
+    public GameObject container;
+
+    //Box button to be interactable
+    public Button btnGC;
+    public Button btnGB;
+
+    //Link the algorithm script
+    private Algorithm algorithm;
+
+    private void Start()
+    {
+        algorithm = GameObject.FindGameObjectWithTag("GameController").GetComponent<Algorithm>();
+    }
+
+    public void GenerateContainer()
+    {
+        if(x.text != "" && y.text != "" && z.text != "")
+        {
+            GameObject containerCreated = container;
+            containerCreated.transform.localScale = new Vector3(float.Parse(x.text), float.Parse(y.text), float.Parse(z.text));
+            GameObject.Instantiate(containerCreated);
+
+            algorithm.SetContainer(containerCreated.transform.localScale);
+
+            btnGC.interactable = false;
+            btnGB.interactable = true;
+        }
+        else
+        {
+            Debug.Log("Input TextField cannot be empty");
+        }
+    }
+
+    public void GsenerateBox()
+    {
+        algorithm.SetBox(new Vector3(float.Parse(x.text), float.Parse(y.text), float.Parse(z.text)));
+    }
+}
