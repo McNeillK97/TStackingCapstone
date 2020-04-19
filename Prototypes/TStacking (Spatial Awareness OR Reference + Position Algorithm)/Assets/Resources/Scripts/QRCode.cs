b75297935cd480e33ebf4c3602ae81408ffdb28c
Sijin Chen <fvjackchen@gmail.com>
2020-04-18 20:41:56 -0700

    Adjust the program flow and combine the QR code

--- Prototypes/TStacking (Spatial Awareness OR Reference + Position Algorithm)/Assets/Resources/Scripts/QRCode.cs
+++ Prototypes/TStacking (Spatial Awareness OR Reference + Position Algorithm)/Assets/Resources/Scripts/QRCode.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System;
+using MySql.Data.MySqlClient;
+using System.Collections;
 
 public class QRCode : MonoBehaviour
 {
@@ -32,5 +34,105 @@ public class QRCode : MonoBehaviour
         width = (float)Convert.ToDouble(IFWidth.text);
         height = (float)Convert.ToDouble(IFHeight.text);
         weight = (float)Convert.ToDouble(IFWeight.text);
+    }
+
+    public void readFromServer()
+    {
+        MySql.Data.MySqlClient.MySqlConnection connection;
+        string server = "remotemysql.com";
+        string database = "CEWfsvQgZu";
+        string uid = "CEWfsvQgZu";
+        string password = "UdelU1N7ef";
+        string connectionString;
+        connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+        database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+        connection = new MySqlConnection(connectionString);
+
+        Queue myBoxQueue = new Queue();
+
+        try
+        {
+            connection.ConnectionString = connectionString;
+            connection.Open();
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM box_info", connection);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            Box myBox;
+            while (reader.Read())
+            {
+                string test1 = reader.GetString("id");
+                string test2 = reader.GetString("length");
+                string test3 = reader.GetString("width");
+                string test4 = reader.GetString("height");
+                string test5 = reader.GetString("weight");
+                myBox = new Box(int.Parse(test1), float.Parse(test2), float.Parse(test3), float.Parse(test4), float.Parse(test5));
+                myBoxQueue.Enqueue(myBox);
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("Error occured. Please try again later.");
+        }
+        connection.Close();
+
+        Debug.Log(myBoxQueue.Count);
+        for(int i = 0; i < 3; i++)
+        {
+            Debug.Log(i);
+            Box box = (Box)myBoxQueue.Dequeue();
+            Debug.Log("Length: " + box.getLength() + " Width: " + box.getWidth() + " Height:" + box.getHeight());
+        }
+    }
+}
+
+class Box
+{
+    private int id;
+    private float length;
+    private float width;
+    private float height;
+    private float weight;
+
+    public Box()
+    {
+        id = 0;
+        length = 0;
+        width = 0;
+        height = 0;
+        weight = 0;
+    }
+
+    public Box(int i, float len, float wid, float hei, float wei)
+    {
+        id = i;
+        length = len;
+        width = wid;
+        height = hei;
+        weight = wei;
+    }
+
+    public int getID()
+    {
+        return id;
+    }
+
+    public float getLength()
+    {
+        return length;
+    }
+
+    public float getWidth()
+    {
+        return width;
+    }
+
+    public float getHeight()
+    {
+        return height;
+    }
+
+    public float getWeight()
+    {
+        return weight;
     }
 }
